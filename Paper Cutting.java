aafee2124dc2396f7cfd8fafed1a83a013a93bfe
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-05 16:26:31 +0530

    Update Paper Cutting.java

--- Paper Cutting.java
+++ Paper Cutting.java
@@ -1,1 +1,22 @@
 Paper Cutting.java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int T=s.nextInt();
+		for(int i=0;i<T;i++)
+		{
+		    int a=s.nextInt();
+		    int b=s.nextInt();
+		    int c=((a/b)*(a/b));
+		    System.out.println(c);
+		}
+
+	}
+}
