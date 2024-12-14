a2017394e1bf68bfacdb7647856641013c4e2bf7
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-14 14:15:43 +0530

    Create chef and socks.java

---
+++ chef and socks.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		int A = input.nextInt();
+		int X = input.nextInt();
+		int Y = input.nextInt();
+		if(X+Y>=A){
+		    System.out.println("YES");
+		}else{
+		    System.out.println("NO");
+		}
+
+	}
+}
