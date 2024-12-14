b28c5a441a23db9641ef97f9b2ea40d915bcc224
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-14 14:09:15 +0530

    Create lucky cover.java

---
+++ lucky cover.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner read = new Scanner(System.in);
+        int N = read.nextInt();
+        System.out.println(4+3*(N-1));
+
+	}
+}
