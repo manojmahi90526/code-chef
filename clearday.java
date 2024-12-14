f03423c08f7b424d10891b30a72f08501c29fc58
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-14 14:23:39 +0530

    Create clearday.java

---
+++ clearday.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		 Scanner scanner = new Scanner(System.in);
+	        
+	        int x = scanner.nextInt();
+	        
+	        int y= scanner.nextInt();
+	     
+	              
+	         int totalweek=7;
+	         
+	         int clearday= totalweek-x-y;
+	     
+	     
+	        System.out.println(clearday);
+	     
+	
+
+	}
+}
