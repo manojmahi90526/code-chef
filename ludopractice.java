867fa49cf0c30096c8dd62f405137112a7b55486
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-16 10:37:41 +0530

    Create ludopractice.java

---
+++ ludopractice.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		while(n-->0)
+		{
+		    int a=s.nextInt();
+		    if(a!=6)
+		    {
+		        System.out.println("NO");
+		    }
+		    else
+		    {
+		        System.out.println("YES");
+		    }
+		}
+
+	}
+}
