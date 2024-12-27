8c8bdc695a461a3f307aecdc481fbab89c09333f
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-27 10:45:47 +0530

    Create election hope.java

---
+++ election hope.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		if(X>=Y*2)
+		{
+		    System.out.println("Yes");
+		    
+		}
+		else
+		{
+		    System.out.println("No");
+		}
+		
+
+	}
+}
