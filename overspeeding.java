92bae6f3ac4f366548cda6cc1a94ff3acc21d16f
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-27 11:12:25 +0530

    Create overspeeding.java

---
+++ overspeeding.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int S=sc.nextInt();
+		if(S>40&&S<=50)
+		{
+		    System.out.println("YES");
+		}
+		else
+		{
+		    System.out.println("NO");
+		}
+
+	}
+}
