306a6978b1d49ca749d001810cef8cead2ecf50d
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-27 10:15:24 +0530

    Create fan pole.java

---
+++ fan pole.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		int C=sc.nextInt();
+		if(A>B&&A>C)
+		{
+		    System.out.println("yes");
+		}
+		else
+		{
+		    System.out.println("No");
+		}
+
+	}
+}
